dfdf8a9bd541e2076a2a9d10c07fa010b0b75bd2
zackhauptman <zhauppy@gmail.com>
2024-11-08 14:46:28 -0500

    Lady bird 3

--- input output exercise/main.js
+++ input output exercise/main.js
@@ -2,18 +2,16 @@ document.getElementById("slider").oninput = function() {
   var size = this.value;
   var paragraphs = document.querySelectorAll("#text p");
 
-  
   paragraphs.forEach((p, index) => {
     if (index < size) {
       p.style.display = "block";
-      p.style.color = getRandomColor();
+      p.style.color = getRandomColor(); 
     } else {
       p.style.display = "none";
     }
   });
-}
+};
 
-// Random Color Generator //
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
